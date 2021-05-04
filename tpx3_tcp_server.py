7f1681bf9ed8c07d899baf3c37f5bb5e009a9693
So Fujinami <s-fujinami@saci.kyoto-u.ac.jp>
2021-05-04 22:05:03 +0900

    fix: fix tpx3 bugs

--- tpx3_tcp_server.py
+++ tpx3_tcp_server.py
@@ -8,11 +8,13 @@
 * ABORT
 * LAST_FRAME
 """
+
 import socketserver
 from threading import Thread
-from bl_tcp_server import BLRequestHandler
+#from bl_tcp_server import BLRequestHandler
 
 import sys
+import array
 
 PORT = 59876
 
@@ -27,28 +29,26 @@ class TPX3RequestHandler(BLRequestHandler):
         global dev
         cmd = cmd.upper()
         if cmd == 'IS_CONNECTED':
-            if dev.isConnected():
-                self.send_text_response('OK is_connected 1')
-            else:
-                self.send_text_response('OK is_connected 0')
+            self.send_text_response('OK is_connected {:d}'.format(dev.isConnected()))
         elif cmd == 'INFO':
             self.send_text_response('OK info {} {} {}'.format(dev.width(), dev.height(), dev.dataType()))
         elif cmd == 'CONFIG':
-            dev.setOperataionMode(pixet.PX_TPX3_OPM_EVENT_ITOT)
+            dev.setOperationMode(pixet.PX_TPX3_OPM_EVENT_ITOT)
             self.send_text_response('OK config')
         elif cmd == 'ACQUIRE':
-            if len(params) != 2:
-                self.send_text_response('ERROR:102 illegal_arguments')
-            # acquire data without file output
-            errno = dev.doSimpleAcquisition(int(params[0]), float(params[1]), pixet.PX_FTYPE_NONE, "")
-            # errno = dev.doSimpleIntegralAcquisition(int(params[0]), float(params[1]), pixet.PX_FTYPE_NONE, "")
-            if errno:
-                self.send_text_response('ERROR:{} acquire.'.format(errno))
-        elif cmd == 'IS_RUNNING':
-            if dev.isAcquisitionRunning():
-                self.send_text_response('OK is_running 1')
+            if len(params) == 2:
+                # acquire data without file output
+                # this method returns after the acquisition is completed
+                errno = dev.doSimpleAcquisition(int(params[0]), float(params[1]), pixet.PX_FTYPE_NONE, "")
+                # errno = dev.doSimpleIntegralAcquisition(int(params[0]), float(params[1]), pixet.PX_FTYPE_NONE, "")
+                if errno:
+                    self.send_text_response('ERROR:{} acquire.'.format(errno))
+                else:
+                    self.send_text_response('OK acquire')
             else:
-                self.send_text_response('OK is_running 0')
+                self.send_text_response('ERROR:102 illegal_arguments')
+        elif cmd == 'IS_RUNNING':
+            self.send_text_response('OK is_running {:d}'.format(dev.isAcquisitionRunning()))
         elif cmd == 'ABORT':
             errno = dev.abortOperation()
             if errno:
@@ -56,7 +56,12 @@ class TPX3RequestHandler(BLRequestHandler):
             else:
                 self.send_text_response('OK abort 0')
         elif cmd == 'LAST_FRAME':
-            frames = dev.lastAcqDataRefInc()
+            _, frame_event = dev.lastAcqFrameRefInc().subFrames()
+            
+            self.send_text_response('OK last_frame int16 {}', frame_event.size())
+            # send binary data
+            self.request.sendall(array.array('h', frame_event.data()))
+
         else:
             self.send_text_response('ERROR:101 unknown_command')
 
@@ -86,11 +91,11 @@ if len(devices) == 0:
 dev = devices[0]
 
 # initialize a server.
-SERVER = socketserver.TCPServer(('', PORT), BLRequestHandler)
+SERVER = socketserver.ThreadingTCPServer(('', PORT), TPX3RequestHandler)
 pixet.registerEvent("Exit", exitCallback, exitCallback)
 
 # set the operation mode EVENT+iTOT
-dev.setOperataionMode(pixet.PX_TPX3_OPM_EVENT_ITOT)
+dev.setOperationMode(pixet.PX_TPX3_OPM_EVENT_ITOT)
 
 # run the server.
 SERVER.serve_forever()
